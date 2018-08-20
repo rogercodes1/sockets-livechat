35de61bf1fe98e962b56b7adcbc5c69430acb197
rogercodes1 <roger.ucsb@gmail.com>
2018-08-20 12:51:58 -0700

    updated server.

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/chat', function(req, res) {
-
-  res.render('chat', {
-    pageTitle: 'Chat',
-    pageID: 'chat'
-  });
-
-});
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+//
+// router.get('/chat', function(req, res) {
+//
+//   res.render('chat', {
+//     pageTitle: 'Chat',
+//     pageID: 'chat'
+//   });
+//
+// });
+//
+// module.exports = router;
